a9b663ef5de34af745876e9afffaac0d968967a5
joy210 <sohanoor404@gmail.com>
2024-01-12 00:31:00 +0600

    update button for md, lg size

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,21 +5,41 @@ type ButtonProps = {
   name: string;
   url: string;
   isIcon?: boolean;
+  isLarge?: boolean;
 };
 
-const ButtonLink: FC<ButtonProps> = ({ url, name, isIcon }) => {
+const ButtonLink: FC<ButtonProps> = ({ url, name, isIcon, isLarge }) => {
   return (
     <Link
       href={url}
-      className={`group h-12 ${
-        isIcon === true ? "pl-[18px] pr-[10px]" : "px-6"
-      } ml-2 font-normal bg-slate-900 text-white rounded-full flex items-center space-x-3 hover:bg-indigo-500 ease-in-out duration-300`}
+      className={`group ml-2 font-normal bg-slate-900 text-white rounded-full inline-flex items-center hover:bg-indigo-500 ease-in-out duration-300 transform translate-y-0 hover:-translate-y-1
+
+
+      ${
+        isIcon === true
+          ? isLarge === true
+            ? buttonProperty.lg
+            : buttonProperty.md
+          : isLarge === true
+          ? "h-14 px-8 text-[18px]"
+          : "h-12 px-6"
+      }
+      
+        `}
     >
       <span> {name} </span>
 
       {isIcon === true && (
-        <span className="rounded-full bg-white h-7 w-7 flex item-center justify-center">
-          <i className="ri-arrow-right-up-line text-[20px] font-semibold text-slate-900 group-hover:text-indigo-500 -mt-[1px]  ease-in-out duration-300" />
+        <span
+          className={`rounded-full bg-white flex item-center justify-center font-semibold text-slate-900 group-hover:text-indigo-500 ease-in-out duration-300 ${
+            isLarge === true ? iconSize.lg : iconSize.md
+          }`}
+        >
+          <i
+            className={`ri-arrow-right-up-line ${
+              isLarge === true ? "-mt-[1px]" : "-mt-[1px]"
+            }`}
+          />
         </span>
       )}
     </Link>
@@ -27,3 +47,14 @@ const ButtonLink: FC<ButtonProps> = ({ url, name, isIcon }) => {
 };
 
 export default ButtonLink;
+
+// Button and Icon property for md, lg size
+const buttonProperty = {
+  md: "h-12 pl-5 pr-2.5 space-x-2",
+  lg: "h-14 text-[18px] pl-6 pr-2.5 space-x-3",
+};
+
+const iconSize = {
+  md: "h-7 w-7 text-[20px]",
+  lg: "h-9 w-9 text-[26px]",
+};
