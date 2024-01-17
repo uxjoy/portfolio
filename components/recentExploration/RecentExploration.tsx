fa16f9664479695665caeb4d4ca6bfd99e413299
joy210 <sohanoor404@gmail.com>
2024-01-18 02:04:57 +0600

    udpated mobile view

--- components/recentExploration/RecentExploration.tsx
+++ components/recentExploration/RecentExploration.tsx
@@ -7,22 +7,22 @@ import {
 
 const RecentExploration = () => {
   return (
-    <div className="bg-slate-950 py-120 relative space-y-20">
-      <div className="container mx-auto">
-        <div className="px-20 text-center">
-          <h2 className="text-[80px] text-slate-800 font-extrabold leading-tight">
+    <div className="bg-slate-950 py-12 pb-16 md:py-120 relative space-y-8 md:space-y-20">
+      <div className="container">
+        <div className="md:px-20 text-center">
+          <h2 className="text-slate-800 font-extrabold leading-tight">
             My Recent {highlightedText("Visual Explorations")}
           </h2>
         </div>
       </div>
 
-      <div className="space-y-8">
+      <div className="space-y-2 md:space-y-8">
         {/* slide to right */}
         <div className="relative flex overflow-x-hidden space-x-8">
-          <div className=" animate-imageSlider whitespace-nowrap flex items-center gap-8">
+          <div className=" animate-imageSlider whitespace-nowrap flex items-center gap-2 md:gap-8">
             {recentExploreData.map(({ title, imgURL }, i) => (
               <div
-                className="min-w-[380px] h-[280px] rounded-xl overflow-hidden border border-slate-900"
+                className="min-w-[148px] md:min-w-[380px] h-[100px] md:h-[280px] rounded-xl overflow-hidden border border-slate-900"
                 key={i}
               >
                 <img
@@ -34,10 +34,10 @@ const RecentExploration = () => {
             ))}
           </div>
 
-          <div className="absolute top-0 animate-imageSlider2 whitespace-nowrap flex items-center gap-8">
+          <div className="absolute top-0 animate-imageSlider2 whitespace-nowrap flex items-center gap-2 md:gap-8">
             {recentExploreData.map(({ title, imgURL }, i) => (
               <div
-                className="min-w-[380px] h-[280px] rounded-xl overflow-hidden border border-slate-900"
+                className="min-w-[148px] md:min-w-[380px] h-[100px] md:h-[280px] rounded-xl overflow-hidden border border-slate-900"
                 key={i}
               >
                 <img
@@ -52,10 +52,10 @@ const RecentExploration = () => {
 
         {/* slide to left */}
         <div className="relative flex overflow-x-hidden space-x-8">
-          <div className=" animate-slidetoright whitespace-nowrap flex items-center gap-8">
+          <div className=" animate-slidetoright whitespace-nowrap flex items-center gap-2 md:gap-8">
             {recentExploreData2.map(({ title, imgURL }, i) => (
               <div
-                className="min-w-[520px] h-[360px] rounded-xl overflow-hidden border border-slate-900"
+                className="min-w-[200px] h-[148px] md:min-w-[380px] md:h-[280px] rounded-xl overflow-hidden border border-slate-900"
                 key={i}
               >
                 <img
@@ -67,10 +67,10 @@ const RecentExploration = () => {
             ))}
           </div>
 
-          <div className="absolute top-0 animate-slidetoright2 whitespace-nowrap flex items-center gap-8">
+          <div className="absolute top-0 animate-slidetoright2 whitespace-nowrap flex items-center gap-2 md:gap-8">
             {recentExploreData2.map(({ title, imgURL }, i) => (
               <div
-                className="min-w-[520px] h-[360px] rounded-xl overflow-hidden border border-slate-900"
+                className="min-w-[200px] h-[148px] md:min-w-[380px] md:h-[280px] rounded-xl overflow-hidden border border-slate-900"
                 key={i}
               >
                 <img
