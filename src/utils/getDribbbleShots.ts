432d2bcb921ca12553c5e40c7429c8cacdccfaed
joy210 <sohanoor404@gmail.com>
2024-01-21 22:54:50 +0600

    update

--- src/utils/getDribbbleShots.ts
+++ src/utils/getDribbbleShots.ts
@@ -1,5 +1,8 @@
 export const getDribbbleShots = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch(
+    "https://api.dribbble.com/v2/user/shots?access_token=" +
+      process.env.ACCESS_TOKEN
+  );
   const data = await res.json();
 
   return data;
