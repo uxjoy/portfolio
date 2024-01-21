b6dc06c7fb5d1bb381eaf463a9d337aa0f6948dd
joy210 <sohanoor404@gmail.com>
2024-01-21 22:56:24 +0600

    test

--- src/utils/getDribbbleShots.ts
+++ src/utils/getDribbbleShots.ts
@@ -1,7 +1,9 @@
+const accessToken =
+  "62f768313c77382aa94dcd655e7a563767839d8b2069308b21b6c9a4a2027707";
+
 export const getDribbbleShots = async () => {
   const res = await fetch(
-    "https://api.dribbble.com/v2/user/shots?access_token=" +
-      process.env.ACCESS_TOKEN
+    "https://api.dribbble.com/v2/user/shots?access_token=" + accessToken
   );
   const data = await res.json();
 
