7a29898674720a17d59ff4ac35af0e3383905a14
Joy210 <sohanoor404@gmail.com>
2024-05-27 13:48:00 +0600

    added code for SEO

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
       },
     ],
   },
+
+
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap.xml',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
---
+++ src/app/pages/api/sitemap.xml.ts
@@ -0,0 +1,36 @@
+// pages/api/sitemap.xml.ts
+
+import { NextApiRequest, NextApiResponse } from "next";
+// import { getAllBlogPosts } from "../../lib/blog"; // Assume you have this function to fetch all blog posts
+
+const Sitemap = async (req: NextApiRequest, res: NextApiResponse) => {
+  // const blogPosts = await getAllBlogPosts();
+
+  const baseUrl = "https://www.example.com";
+  const staticPages = [
+    "/",
+    // "/about",
+    // "/contact",
+    // "/blog",
+    // Add more static pages here
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticPages
+      .map((url) => {
+        return `
+          <url>
+            <loc>${baseUrl}${url}</loc>
+          </url>
+        `;
+      })
+      .join("")}
+  </urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.send(sitemap);
+  // res.end();
+};
+
+export default Sitemap;
