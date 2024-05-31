afe536ffa0e181ce1873fb471d59d15039b47538
joy210 <sohanoor404@gmail.com>
2024-05-31 14:10:14 +0600

    new resume

--- components/data/navbarData.ts
+++ components/data/navbarData.ts
@@ -1,7 +1,7 @@
 // Navbar menu items
 
 // ("https://drive.google.com/file/d/1W2_HCbalyu-ygi-HOvcWk49JHq2B7eVV/view?usp=sharing");
-export const resumeURL = "http://tinyurl.com/3bu43wyh";
+export const resumeURL = "https://drive.google.com/file/d/1Uv6MElaRGIFgd_Y1LfCo7nNorPIAJXdl/view?usp=sharing";
 
 // Menu item data
 export const menuItem = [
