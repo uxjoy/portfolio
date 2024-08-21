f72fe525af40fd08b8995b14b2cc2e349cf3cc2e
joy210 <sohanoor404@gmail.com>
2024-08-21 23:47:45 +0600

    update

--- src/app/case-studies/[caseStudyId]/page.tsx
+++ src/app/case-studies/[caseStudyId]/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import Contact from "../../../../components/Contact";
 import Navabar from "../../../../components/navbar/Navabar";
+import branding from "../../../../public/assets/case-study/branding.png";
 import imgLandscape from "../../../../public/assets/case-study/ebl-overview.jpg";
 import imgUrl_1 from "../../../../public/assets/case-study/img_1.png";
 import imgUrl_2 from "../../../../public/assets/case-study/img_2.png";
 import imgUrl_3 from "../../../../public/assets/case-study/img_3.png";
 import imgUrl_4 from "../../../../public/assets/case-study/img_4.png";
-import imgUrl_5 from "../../../../public/assets/case-study/iPhone_&_Book_Cover.png";
 import imgUrl_6 from "../../../../public/assets/case-study/img_6.png";
 
 const CaseStudyDetails = ({ params }: any) => {
@@ -30,20 +30,16 @@ const CaseStudyDetails = ({ params }: any) => {
         <div className="container relative space-y-6 md:space-y-2">
           <div className="top-content w-3/4">
             <div className="uppercase text-lg text-slate-400">case study</div>
-            <h1
-              className="h3 capitalize font-bold text-slate-400 w-full mt-4"
-              style={{ lineHeight: 1.3 }}
-            >
-              <span className="text-slate-900">EBL Fund Transfer</span> redesign
-              for mobile app
+            <h1 className="h3 capitalize font-bold text-slate-400 w-full mt-4" style={{ lineHeight: 1.3 }}>
+              <span className="text-slate-900">EBL Fund Transfer</span> redesign for mobile app
             </h1>
           </div>
         </div>
       </div>
 
-      <div className="relative w-full h-[380px] md:h-[680px] bg-slate-200 overflow-hidden">
+      <div className="relative w-full h-[380px] md:h-[920px] bg-white overflow-hidden">
         <Image
-          className="object-cover w-full h-full group-hover:scale-110 duration-300"
+          className="object-contain h-full group-hover:scale-110 duration-300"
           src={imgLandscape}
           alt={"title"}
           fill
@@ -76,9 +72,7 @@ const CaseStudyDetails = ({ params }: any) => {
             <div className="space-y-2 w-full">
               <label className="text-slate-500">Figma</label>
               <Link
-                href={
-                  "https://www.figma.com/design/ujYWr8iLm1RuZYI1jku0LH/EBL-%3A-Fund-Transfer?node-id=158-4817"
-                }
+                href={"https://www.figma.com/design/ujYWr8iLm1RuZYI1jku0LH/EBL-%3A-Fund-Transfer?node-id=158-4817"}
                 className="font-semibold text-xl block underline underline-offset-4"
               >
                 Click Here
@@ -94,26 +88,107 @@ const CaseStudyDetails = ({ params }: any) => {
                 Project <br /> Overview
               </div>
               <p className="w-full text-xl font-normal text-slate-700  leading-relaxed">
-                <span className="font-medium text-slate-900">
-                  {" "}
-                  The EBL Fund Transfer feature{" "}
-                </span>
-                required a significant redesign to address usability issues and
-                improve the overall user experience. The existing feature was
-                cluttered and lacked clear instructions, causing confusion and
-                errors during the transfer process. The redesign aimed to create
-                an intuitive, modern, and user-friendly interface, focusing on
-                fund transfer UX design and mobile banking optimization.
+                <span className="font-medium text-slate-900"> The EBL Fund Transfer feature </span>
+                required a significant redesign to address usability issues and improve the overall user experience. The
+                existing feature was cluttered and lacked clear instructions, causing confusion and errors during the
+                transfer process. The redesign aimed to create an intuitive, modern, and user-friendly interface,
+                focusing on fund transfer UX design and mobile banking optimization.
               </p>
             </div>
           </div>
 
-          <div className="divider h-[1px] w-full bg-slate-100 my-24"></div>
+          {/* <div className="divider h-[1px] w-full bg-slate-100 my-24"></div> */}
+        </div>
+      </div>
 
-          <div className="iframe w-full h-[500px] space-y-8">
+      <div className="problems-and-solutions mt-16 bg-indigo-500 text-white py-12 md:py-[160px]">
+        <div className="container space-y-32">
+          <div className="flex gap-40">
             <div className="flex-initial w-6/12 text-3xl font-semibold">
-              User Flow
+              Problem <br /> Identification
             </div>
+
+            <div className="right w-full space-y-4 text-xl font-light text-white">
+              <div className="flex gap-4">
+                <span>
+                  <RiCloseLine size={24} />
+                </span>
+
+                <span>Cluttered interface with poor spacing and visual hierarchy.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCloseLine size={24} />
+                </span>
+                <span>Lack of clear instructions, particularly for new transfer options.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCloseLine size={24} />
+                </span>
+                <span>Inadequate validation allowed incorrect account number entries.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCloseLine size={24} />
+                </span>
+                <span>Redundant call-to-actions cluttered the interface.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCloseLine size={24} />
+                </span>
+                <span>Transfer mode options were confusing and poorly explained.</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-40">
+            <div className="flex-initial w-6/12 text-3xl font-semibold">
+              Design <br /> Solutions
+            </div>
+
+            <div className="right w-full space-y-4 text-xl font-light text-white">
+              <div className="flex gap-4">
+                <span>
+                  <RiCheckLine size={24} />
+                </span>
+
+                <span>Simplified layout with a modern UI to improve focus and usability.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCheckLine size={24} />
+                </span>
+                <span>Introduced clear separation of transfer options and added instructional tooltips.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCheckLine size={24} />
+                </span>
+                <span>Implemented validation for 13-digit account numbers and improved error messaging.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCheckLine size={24} />
+                </span>
+                <span>Streamlined navigation by removing unnecessary CTAs.</span>
+              </div>
+              <div className="flex gap-4">
+                <span>
+                  <RiCheckLine size={24} />
+                </span>
+                <span>Redesigned the transfer mode modal with clear options and explanations.</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="iframe py-12 md:py-120 md:mb-12">
+        <div className="container">
+          <div className="w-full h-[500px] space-y-8">
+            <div className="flex-initial w-6/12 text-3xl font-semibold">User Flow</div>
 
             <iframe
               width="100%"
@@ -125,118 +200,10 @@ const CaseStudyDetails = ({ params }: any) => {
         </div>
       </div>
 
-      <div className="problems-and-solutions mt-16 bg-slate-950 text-white py-12 md:py-[160px]">
-        <div className="container space-y-32">
-          <div className="flex gap-40">
-            <div className="flex-initial w-6/12 text-3xl font-medium">
-              Problem <br /> Identification
-            </div>
-
-            <div className="right w-full space-y-4 text-xl font-light text-slate-300">
-              <div className="flex gap-4">
-                <span>
-                  <RiCloseLine size={24} className="text-amber-500" />
-                </span>
-
-                <span>
-                  Cluttered interface with poor spacing and visual hierarchy.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCloseLine size={24} className="text-amber-500" />
-                </span>
-                <span>
-                  Lack of clear instructions, particularly for new transfer
-                  options.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCloseLine size={24} className="text-amber-500" />
-                </span>
-                <span>
-                  Inadequate validation allowed incorrect account number
-                  entries.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCloseLine size={24} className="text-amber-500" />
-                </span>
-                <span>Redundant call-to-actions cluttered the interface.</span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCloseLine size={24} className="text-amber-500" />
-                </span>
-                <span>
-                  Transfer mode options were confusing and poorly explained.
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex gap-40">
-            <div className="flex-initial w-6/12 text-3xl font-medium">
-              Design <br /> Solutions
-            </div>
-
-            <div className="right w-full space-y-4 text-xl font-light text-slate-300">
-              <div className="flex gap-4">
-                <span>
-                  <RiCheckLine size={24} className="text-green-400" />
-                </span>
-
-                <span>
-                  Simplified layout with a modern UI to improve focus and
-                  usability.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCheckLine size={24} className="text-green-400" />
-                </span>
-                <span>
-                  Introduced clear separation of transfer options and added
-                  instructional tooltips.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCheckLine size={24} className="text-green-400" />
-                </span>
-                <span>
-                  Implemented validation for 13-digit account numbers and
-                  improved error messaging.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCheckLine size={24} className="text-green-400" />
-                </span>
-                <span>
-                  Streamlined navigation by removing unnecessary CTAs.
-                </span>
-              </div>
-              <div className="flex gap-4">
-                <span>
-                  <RiCheckLine size={24} className="text-green-400" />
-                </span>
-                <span>
-                  Redesigned the transfer mode modal with clear options and
-                  explanations.
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="relative w-full h-[380px] md:h-[640px] bg-slate-200 overflow-hidden">
+      <div className="relative w-full h-[380px] md:h-[700px]  2xl:h-[1080px]  bg-slate-200 overflow-hidden">
         <Image
           className="object-cover w-full h-full group-hover:scale-110 duration-300"
-          src={imgUrl_5}
+          src={branding}
           alt={"title"}
           fill
           priority={false}
@@ -248,9 +215,7 @@ const CaseStudyDetails = ({ params }: any) => {
       <div className="py-12 md:py-120">
         <div className="container space-y-[120px]">
           <div className="design-process flex gap-40">
-            <div className="flex-initial w-6/12 text-3xl font-semibold">
-              Design Process
-            </div>
+            <div className="flex-initial w-6/12 text-3xl font-semibold">Design Process</div>
 
             <div className="w-fullfont-normal text-slate-700 space-y-8 leading-relaxed">
               <div className="pl-12 relative">
@@ -258,12 +223,10 @@ const CaseStudyDetails = ({ params }: any) => {
                   <RiCheckLine size={26} />
                 </span>
 
-                <h4 className="text-xl text-slate-900 font-medium mb-2">
-                  Research & Analysis:
-                </h4>
+                <h4 className="text-xl text-slate-900 font-medium mb-2">Research & Analysis:</h4>
                 <p>
-                  I conducted a comprehensive analysis of user feedback and pain
-                  points to identify key areas for improvement.
+                  I conducted a comprehensive analysis of user feedback and pain points to identify key areas for
+                  improvement.
                 </p>
               </div>
 
@@ -272,13 +235,10 @@ const CaseStudyDetails = ({ params }: any) => {
                   <RiCheckLine size={26} />
                 </span>
 
-                <h4 className="text-xl text-slate-900 font-medium mb-2">
-                  Wireframing & Prototyping:
-                </h4>
+                <h4 className="text-xl text-slate-900 font-medium mb-2">Wireframing & Prototyping:</h4>
                 <p>
-                  Developed wireframes and prototypes to explore and refine
-                  design solutions, ensuring alignment with user needs and
-                  preferences.
+                  Developed wireframes and prototypes to explore and refine design solutions, ensuring alignment with
+                  user needs and preferences.
                 </p>
               </div>
 
@@ -287,13 +247,10 @@ const CaseStudyDetails = ({ params }: any) => {
                   <RiCheckLine size={26} />
                 </span>
 
-                <h4 className="text-xl text-slate-900 font-medium mb-2">
-                  User Testing:
-                </h4>
+                <h4 className="text-xl text-slate-900 font-medium mb-2">User Testing:</h4>
                 <p>
-                  Performed usability testing with a representative sample of
-                  users to gather insights and fine-tune the design for optimal
-                  user satisfaction.
+                  Performed usability testing with a representative sample of users to gather insights and fine-tune the
+                  design for optimal user satisfaction.
                 </p>
               </div>
 
@@ -302,13 +259,10 @@ const CaseStudyDetails = ({ params }: any) => {
                   <RiCheckLine size={26} />
                 </span>
 
-                <h4 className="text-xl text-slate-900 font-medium mb-2">
-                  Final Implementation:
-                </h4>
+                <h4 className="text-xl text-slate-900 font-medium mb-2">Final Implementation:</h4>
                 <p>
-                  The final design was implemented with a focus on maintaining
-                  consistency, enhancing usability, and presenting a modern,
-                  appealing aesthetic.
+                  The final design was implemented with a focus on maintaining consistency, enhancing usability, and
+                  presenting a modern, appealing aesthetic.
                 </p>
               </div>
             </div>
@@ -381,18 +335,13 @@ const CaseStudyDetails = ({ params }: any) => {
       <div className="py-12 md:py-120 bg-slate-50">
         <div className="container space-y-[120px]">
           <div className="flex gap-40">
-            <div className="flex-initial w-6/12 text-3xl font-semibold">
-              Outcome
-            </div>
+            <div className="flex-initial w-6/12 text-3xl font-semibold">Outcome</div>
             <p className="w-full text-xl font-normal text-slate-700  leading-relaxed">
-              The redesigned EBL Fund Transfer feature now offers a
-              significantly enhanced user experience, with improved navigation,
-              clearer instructions, and better error handling. The modernized
-              interface aligns with contemporary mobile banking standards and
-              addresses key usability issues, resulting in a more efficient and
-              user-friendly fund transfer process. This redesign not only
-              modernized the interface but also ensured it met user needs and
-              expectations, boosting user satisfaction and reducing errors.
+              The redesigned EBL Fund Transfer feature now offers a significantly enhanced user experience, with
+              improved navigation, clearer instructions, and better error handling. The modernized interface aligns with
+              contemporary mobile banking standards and addresses key usability issues, resulting in a more efficient
+              and user-friendly fund transfer process. This redesign not only modernized the interface but also ensured
+              it met user needs and expectations, boosting user satisfaction and reducing errors.
             </p>
           </div>
         </div>
--- src/app/pages/api/sitemap.xml.ts
+++ src/app/pages/api/sitemap.xml.ts
@@ -18,9 +18,9 @@ const Sitemap = async (req: NextApiRequest, res: NextApiResponse) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${staticPages
-      .map((url) => {
+      .map((url, i) => {
         return `
-          <url>
+          <url key=${i}>
             <loc>${baseUrl}${url}</loc>
           </url>
         `;
