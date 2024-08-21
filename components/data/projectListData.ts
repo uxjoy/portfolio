ec363ba6c274b525a2e0ea285797c3a079aa7c3a
Joy210 <sohanoor404@gmail.com>
2024-08-21 16:51:13 +0600

    update

--- components/data/projectListData.ts
+++ components/data/projectListData.ts
@@ -13,21 +13,21 @@ export const projectList = [
     imageURL: "/assets/PetraUI_1.jpg",
   },
   {
-    url: "https://dribbble.com/uxjoy_",
-    title: "Cypto Currency App Design",
-    tag: "Mobile App Design",
-    imageURL: "/assets/Crypto_App_1.jpg",
+    url: "case-studies/ebl-fund-transfer-redesign",
+    title: "EBL Fund Transfer Redesign",
+    tag: "Mobile App",
+    imageURL: "/assets/case-study/img_6.png",
   },
+  // {
+  //   url: "https://dribbble.com/uxjoy_",
+  //   title: "Cypto Currency App Design",
+  //   tag: "Mobile App Design",
+  //   imageURL: "/assets/Crypto_App_1.jpg",
+  // },
   {
     url: "https://dribbble.com/uxjoy_",
     title: "ExitFrog (Internal Learning Platform)",
     tag: "Website Design",
     imageURL: "/assets/ExitFrog.jpg",
-  },
-  {
-    url: "case-studies/ebl-fund-transfer-redesign",
-    title: "EBL Fund Transfer Redesign",
-    tag: "Mobile App",
-    imageURL: "/assets/case-study/img_6.png",
   },
 ];
