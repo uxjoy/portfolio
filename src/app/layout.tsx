6e4214242323513d2fcd4753da84839ee71e9f1d
joy210 <sohanoor404@gmail.com>
2024-10-08 23:40:49 +0600

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { Analytics } from "@vercel/analytics/react";
-import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
 
 // Import Swiper styles
+import { Metadata } from "next";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -11,10 +11,10 @@ import "swiper/css/pagination";
 const font = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sohanur Rahman (Joy) | Product Designer",
+  title: "Sohanur Rahman (Joy) | Sr. Product Designer",
   description:
     "Crafting modern, user-friendly designs with seamless precision, driven by an unwavering passion for my craft, I am dedicated to delivering contemporary, user-centric design while maintaining a profound enthusiasm for my work.",
-  // metadataBase: new URL("/assets/Meta_Image.jpg"),
+  // keywords: "https://www.uxjoy.info",
 };
 
 const person = {
@@ -41,20 +41,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        {/* <meta
-          property="og:title"
-          content="Explore the Creative Universe of Sohanur Rahman (Joy) | UX/UI Design Portfolio"
-        />
-        <meta
-          property="og:description"
-          content="I am Sohanur Rahman (Joy), Product Designer. I derive immense satisfaction from crafting experiences that prioritize users, infusing them with delight and a genuine human touch. My mission is to propel society forward by forging a seamless connection between individuals and technology, fostering understanding and innovation."
-        /> */}
         <meta name="next-size-adjust" />
-        <meta property="og:title" content="Sohanur Rahman (Joy) | Product Designer" />
+        {/* <meta property="og:title" content="Sohanur Rahman (Joy) | Product Designer" />
         <meta
           property="og:description"
           content="Crafting modern, user-friendly designs with seamless precision, driven by an unwavering passion for my craft, I am dedicated to delivering contemporary, user-centric design while maintaining a profound enthusiasm for my work."
-        />
+        /> */}
         <meta property="og:url" content="https://www.uxjoy.info" />
         <meta property="og:image" content="/assets/Meta_Image.jpg" />
         <meta property="og:site_name" content="Sohanur Rahman (Joy) | Product Designer" />
@@ -62,6 +54,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           property="og:keywords"
           content="Sohanur Rahman (Joy), Product Designer, UX Designer, User Centered Product Designer, User Experience Designer, Human Experience Designer, Problem Solver, Researcher, AR & VR Enthusiast, joy210, _joy, sohanoor, uxjoy, Sohanur Rahman Joy, sohanoor, sohanur, Sohanur Rahman Joy - Product Designer, Dhaka, Bangladesh"
         />
+        <meta name="twitter:card" content="summary_large_image" />
+
         <meta property="og:type" content="website" />
 
         <meta
