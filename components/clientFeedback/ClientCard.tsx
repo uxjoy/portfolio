ddee1e261f79b0a14c2e788bcd2c00ec922292ea
Joy210 <sohanoor404@gmail.com>
2025-02-17 16:08:37 +0600

    update

--- components/clientFeedback/ClientCard.tsx
+++ components/clientFeedback/ClientCard.tsx
@@ -7,7 +7,7 @@ type FeedbackDataProps = {
 
 const ClientCard: FC<FeedbackDataProps> = ({ data }) => {
   return (
-    <div className="relative space-y-4 text-gray-600 dark:text-white/70 bg-white/5 border border-white/5 p-6 rounded-2xl overflow-hidden cursor-default group transform translate-y-0 hover:-translate-y-1.5  ease-in-out duration-300">
+    <div className="relative space-y-4 text-gray-600 dark:text-whiteAlpha-72 bg-whiteAlpha-4 border border-whiteAlpha-4 p-6 rounded-2xl overflow-hidden cursor-default group transform translate-y-0 hover:-translate-y-1.5  ease-in-out duration-300">
       <RiDoubleQuotesR size={32} color="#9AE66E" />
 
       <p className="font-light text-base leading-normal group-hover:text-primary-default">
@@ -18,30 +18,12 @@ const ClientCard: FC<FeedbackDataProps> = ({ data }) => {
         <p className="text-base font-semibold text-gray-900 dark:text-white dark:font-medium">
           {data.name}
         </p>
-        <p className="text-sm dark:text-white/50"> {data.location} </p>
+        <p className="text-sm dark:text-whiteAlpha-56"> {data.location} </p>
       </div>
 
-      <div className="absolute -top-8 -right-6 w-12 h-12 bg-primary-default blur-3xl invisible group-hover:visible ease-in-out duration-200 delay-150"></div>
+      <div className="absolute -top-8 -right-6 w-12 h-12 bg-primary-default blur-3xl invisible group-hover:visible"></div>
     </div>
   );
 };
 
 export default ClientCard;
-
-{
-  /* <div className="space-y-4 md:space-y-10 text-gray-600 dark:text-stone-400">
-  <p className="text-[13px] md:text-2xl font-light leading-normal">
-    {data.quote}
-  </p>
-
-  <div className="space-y-1 md:space-y-1">
-    <p className="text-sm md:text-xl font-semibold text-gray-900 dark:text-stone-300 dark:font-medium">
-      {data.name}
-    </p>
-    <p className=" text-xs md:text-base dark:text-stone-500">
-      {" "}
-      {data.location}{" "}
-    </p>
-  </div>
-</div>; */
-}
