9b22e2b8d5934950c97ba927ff9b8c7bd90840d1
joy210 <sohanoor404@gmail.com>
2025-02-17 21:45:05 +0600

    client feedback udpated

--- components/clientFeedback/ClientsFeedback.tsx
+++ components/clientFeedback/ClientsFeedback.tsx
@@ -1,7 +1,7 @@
 "use client";
+import { RiStarFill } from "@remixicon/react";
 import { useSwiper } from "swiper/react";
 import { clientFeedbackList } from "../../src/utils/data/clientData";
-import Heading from "../Heading";
 import ClientCard from "./ClientCard";
 
 const ClientsFeedback = () => {
@@ -9,10 +9,25 @@ const ClientsFeedback = () => {
 
   return (
     <div className="py-12 md:py-120">
-      <div className="container space-y-8 md:space-y-20">
-        <Heading text1="Clients" text2="Feedback" />
+      <div className="container space-y-4">
+        {/* <Heading text1="Clients" text2="Feedback" /> */}
 
-        <div className="columns-3 gap-6 space-y-6 w-full">
+        <div className="flex items-center justify-center font-light gap-3">
+          <p className="text-yellow-500 flex items-center gap-2">
+            <RiStarFill size={18} /> <span className="text-white mt-0.5 font-medium">5.0</span>
+          </p>
+          <div className="line w-0.5 h-4 bg-whiteAlpha-8"></div>
+          <p className="mt-0.5 text-whiteAlpha-72 font-extralight">Top 10% of Talent on Upwork</p>
+        </div>
+
+        <div className="section-header text-center">
+          <h1 className="text-[56px] font-bold flex flex-col leading-tight">
+            <span>Look at some of the </span>
+            <span className="text-primary-default">impact I've made for others</span>
+          </h1>
+        </div>
+
+        <div className="columns-3 gap-6 space-y-6 w-full pt-10">
           {clientFeedbackList &&
             clientFeedbackList.map((data, i) => (
               <div className="w-full break-inside-avoid" key={i}>
