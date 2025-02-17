bdedeb3074ca48db32d0fd33dc1cb50df5f0acf7
joy210 <sohanoor404@gmail.com>
2025-02-17 22:48:19 +0600

    update color

--- components/marquee/BigTextMarquee.tsx
+++ components/marquee/BigTextMarquee.tsx
@@ -5,15 +5,9 @@ import MarqueeAnimation from "react-fast-marquee";
 const BigTextMarquee = () => {
   return (
     <>
-      <div className={`leading-none -space-y-4`}>
-        <div className="text-5xl md:text-8xl uppercase font-semibold space-x-4 text-gray-200 dark:text-primary-50/5 overflow-hidden">
-          <MarqueeAnimation
-            speed={50}
-            pauseOnHover={false}
-            play={true}
-            direction="left"
-            className="overflow-hidden"
-          >
+      <div className={`leading-none -space-y-4 opacity-[3%]`}>
+        <div className="text-5xl md:text-8xl uppercase font-semibold space-x-4 text-gray-200 dark:text-primary-50 overflow-hidden">
+          <MarqueeAnimation speed={50} pauseOnHover={false} play={true} direction="left" className="overflow-hidden">
             {marqueeTexts.map((text, i) => {
               return (
                 <h1 key={i} className="flex items-center gap-4">
@@ -42,57 +36,6 @@ const BigTextMarquee = () => {
           </MarqueeAnimation>
         </div>
       </div>
-
-      {/* <div className="leading-none -space-y-4">
-      <div className="relative flex overflow-x-hidden text-5xl md:text-8xl uppercase font-semibold space-x-4 text-gray-200 dark:text-stone-900/70">
-        <div className="py-4 animate-slidtoleft whitespace-nowrap flex items-center gap-4 ">
-          {marqueeTextsBig.map((text, i) => {
-            return (
-              <p key={i} className="flex items-center gap-4">
-                {text}
-                <RiShining2Fill size={40} />
-              </p>
-            );
-          })}
-        </div>
-
-        <div className="absolute top-0 py-4 animate-slidtoleft2 whitespace-nowrap flex items-center gap-4">
-          {marqueeTextsBig.map((text, i) => {
-            return (
-              <p key={i} className="flex items-center gap-4">
-                {text}
-                <RiShining2Fill size={40} />
-              </p>
-            );
-          })}
-        </div>
-      </div>
-
-      <div className="relative flex overflow-x-hidden text-5xl md:text-8xl uppercase font-semibold space-x-4 text-gray-200 dark:text-stone-900/70">
-        <div className="py-4 animate-slidetoright whitespace-nowrap flex items-center gap-4 ">
-          {marqueeTextsBig.map((text, i) => {
-            return (
-              <p key={i} className="flex items-center gap-4">
-                {text}
-                <RiShining2Fill size={40} />
-              </p>
-            );
-          })}
-        </div>
-
-        <div className="absolute top-0 py-4 animate-slidetoright2 whitespace-nowrap flex items-center gap-4">
-          {marqueeTextsBig.map((text, i) => {
-            return (
-              <p key={i} className="flex items-center gap-4">
-                {text}
-
-                <RiShining2Fill size={40} />
-              </p>
-            );
-          })}
-        </div>
-      </div>
-    </div> */}
     </>
   );
 };
