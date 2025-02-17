07690934104136dcc819d0ddee555083db63a472
joy210 <sohanoor404@gmail.com>
2025-02-17 23:06:25 +0600

    update dribbble projects

--- components/recentExploration/RecentExploration.tsx
+++ components/recentExploration/RecentExploration.tsx
@@ -1,24 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
+import Divider from "../Divider";
 import ImageSlider from "./ImageSlider";
 
 const RecentExploration = () => {
   return (
-    <div className="bg-slate-400/5 dark:bg-zinc-900/40 pb-12 md:pb-120 relative space-y-9 md:space-y-24">
-      {/* <div className="container">
-        <div className="md:px-20 text-center">
-          <div className="h2 text-slate-800 font-extrabold leading-tight">
-            My Recent {highlightedText("Visual Explorations")}
-          </div>
-        </div>
-      </div> */}
+    <div className="bg-slate-400/5 dark:bg-transparent pb-12 md:pb-120 relative space-y-9 md:space-y-24">
       <div className="container">
-        <div className="divider w-full flex items-center gap-4 text-center">
-          <span className="w-full h-[1px] bg-gray-200 dark:bg-stone-800"></span>
-          <span className="text-nowrap md:text-lg dark:font-light w-auto text-gray-400 dark:text-stone-700">
-            My recent visual Explorations
-          </span>
-          <span className="w-full h-[1px] bg-gray-200 dark:bg-stone-800"></span>
-        </div>
+        <Divider starText="other projects" centerText="visual explorations" endText="side projects" />
       </div>
 
       <div className="space-y-2 md:space-y-2">
